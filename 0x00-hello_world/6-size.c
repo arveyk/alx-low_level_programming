f453c27f9fe290df888fd3b93375060ccb9660ac
arveyk <harveykisiangani@gmail.com>
2022-12-08 14:03:36 +0300

    intel 100 assembly code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,11 +15,11 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("Size of a char: %c byte(s)\n", sizeof(c));
+	printf("Size of a char: %d byte(s)\n", sizeof(c));
 	printf("Size of an int: %d byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s\n)", sizeof(li));
-	printf("Size of a long long int: %lld byte(s)\n", sizeof(lli));
-	printf("Size of a float: %f byte(s)\n", sizeof(f));
+	printf("Size of a long int: %d byte(s\n)", sizeof(li));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+	printf("Size of a float: %d byte(s)\n", sizeof(f));
 
 	return (0);
 }
