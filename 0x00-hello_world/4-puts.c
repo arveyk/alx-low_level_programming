5760be207be8ef15fdbc4a35e401299911946e09
arveyk <harveykisiangani@gmail.com>
2022-12-08 16:46:19 +0300

    \n removed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 
-	char puzzle[] = "\"Programming is like building a multilingual puzzle\n";
+	char puzzle[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(puzzle);
 
