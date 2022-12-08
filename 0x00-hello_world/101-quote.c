07d1e68970e1341bd76a3e406650a7d1518a570f
arveyk <harveykisiangani@gmail.com>
2022-12-08 23:11:01 +0300

    101quotes .02

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@ int main(void)
 {
 
 
-	char c[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-11\n";
+	char c[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, c, 60);
 
