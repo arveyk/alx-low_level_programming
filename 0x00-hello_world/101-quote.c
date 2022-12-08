e08de91662d6fea11c2d062ca08c897c2d9028d9
arveyk <harveykisiangani@gmail.com>
2022-12-08 14:53:10 +0300

    file diescripter 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 
-	write(1, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 60);
+	write(2, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 60);
 
 	return (1);
 }
