955bd27fd35e84f0754ba02dce2cf24d7f481251
arveyk <harveykisiangani@gmail.com>
2022-12-08 20:36:19 +0300

    Printing without printing functions

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@ int main(void)
 {
 
 
-	char c[60] = "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n";
-	
+	char c[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	return (*c);
 
-i}
+}
