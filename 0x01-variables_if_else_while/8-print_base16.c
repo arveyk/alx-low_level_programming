4ebe674c8b25c4a2d26f77fde19ce4f71e00372d
arveyk <harveykisiangani@gmail.com>
2022-12-09 23:31:59 +0300

    hexample

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+
+
+	int n;
+
+	for (n = 0; n < 10; n++)
+
+		putchar(n + '0');
+
+
+	for (n = 'a'; n <= 'f'; n++)
+
+		putchar(n);
+	
+
+	putchar('\n');
+
+	return (0);
+}
