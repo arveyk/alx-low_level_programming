cba79bcd1a73bfbfe6f58f6ecb51316ca82ac6dc
arveyk <harveykisiangani@gmail.com>
2022-12-13 20:49:23 +0300

    Alphabet lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the lowercase letters
+ *
+ * Return: Does not have return value
+ */
+void print_alphabet(void)
+{
+
+	int q;
+
+	q = 'a';
+
+	while (q <= 'z')
+	{
+		_putchar(q);
+		q++;
+	}
+	_putchar('\n');
+
+	return;
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,9 @@
-#ifndef(MAIN.H)
-#define(MAIN.H)
+#ifndef		MAIN_H
 
+#define		MAIN_H
+
+int _putchar(char c);
 void print_alphabet(void);
+
 
 #endif
