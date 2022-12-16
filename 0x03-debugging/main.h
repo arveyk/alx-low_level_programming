76bfb87a3ff4a103e8d1dc012f364f7a0c57c26a
arveyk <harveykisiangani@gmail.com>
2022-12-16 18:11:17 +0300

    Better Days check

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,7 +6,8 @@
 void positive_or_negative(int i);
 
 int largest_number(int, int, int);
-void print_remaining_days(int, int, int);
-int convert_day(int, int);
+void print_remaining_days(int day, int month, int year);
+int convert_day(int month, int day);
+
 
 #endif /* MAIN_H */
