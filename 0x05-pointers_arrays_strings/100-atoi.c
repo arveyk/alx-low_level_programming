384c8be7385ea776f93015019e1ce28caa5947cd
arveyk <harveykisiangani@gmail.com>
2022-12-20 01:43:39 +0300

    some attempts

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "string.h"
+/**
+ * _atoi - converts string to integers
+ *
+ * Return: integer type
+ * @s: pointer to string to be converted
+ */
+int _atoi(char *s)
+{
+
+	int t = strlen(s), i;
+
+		for (i = 0; i < t; i++)
+		{
+
+			t = t * 10 + (s[i] - '0');
+		}
+	return (t);
+}
