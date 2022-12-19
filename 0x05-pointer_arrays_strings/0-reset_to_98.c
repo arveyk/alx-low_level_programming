9a8093e08a0dfe49217f6c81121d3f2203d1402d
Harvey <harveykisiangani@gmail.com>
2022-12-19 03:28:20 -0800

    modfy variable value using pointer

---
+++ 0x05-pointer_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the value of varible remotely
+ *
+ * Return: void
+ * @n: pointer variable used to mod
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
