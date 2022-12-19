3b5967cbe48592d8462b6a19b2e85bb641438f6d
arveyk <harveykisiangani@gmail.com>
2022-12-20 02:18:36 +0300

    string coppied

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,14 +5,17 @@
  * *_strcpy - copies string including the null character
  *
  * Return: pointer to dest
+ * @dest: pointer to variable to be written to
+ * @src: string to be copied
  */
 char *_strcpy(char *dest, char *src)
 {
 
-	unsigned long int z;
-
-	for (z = 0; z <= (sizeof(*src)); z++)
-		*dest = *src;
-
-	return (dest);
+char *ptr = dest;
+	while ((*dest = *src) != '\0')
+	{
+		dest++;
+		src++;
+	}
+	return (ptr);
 }
