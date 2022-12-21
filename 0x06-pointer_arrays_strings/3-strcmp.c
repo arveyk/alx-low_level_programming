a65b6fa3e359d0a1f70ffc1c4e11c5313bd22c12
arveyk <harveykisiangani@gmail.com>
2022-12-21 19:19:02 +0300

    betty cheki

--- 0x06-pointer_arrays_strings/3-strcmp.c
+++ 0x06-pointer_arrays_strings/3-strcmp.c
@@ -4,13 +4,13 @@
  * _strcmp - compares strings
  *
  * Return: number of byte indicating diffence.
- * s1: 1st string
- * s2: string to be compared to first.
+ * @s1: 1st string
+ * @s2: string to be compared to first.
  */
 int _strcmp(char *s1, char *s2)
 {
 
-	for ( ;*s1 == *s2; s1++, s2++)
+	for ( ; *s1 == *s2; s1++, s2++)
 	{
 		if (*s1 == *s2)
 			return (0);
