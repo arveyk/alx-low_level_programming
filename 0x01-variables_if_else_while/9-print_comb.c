c545e378112e3346b44d8fd9d3c54d3e90e31125
Harvey <harveykisiangani@gmail.com>
2022-12-22 09:48:51 -0800

    comb 9 solve

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,11 +12,14 @@ int main(void)
 	int n;
 
 
-	while(n < 10)
+	while (n < 10)
 	{
 		putchar(n + '0');
-		putchar(',');
-		putchar(' ');
+		if (n < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		n++;
 	}
 	putchar('\n');
