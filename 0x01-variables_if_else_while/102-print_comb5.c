1440981fd0cd5786e2f5b4bc980fb8d3d1e5273a
Harvey <harveykisiangani@gmail.com>
2022-12-24 09:11:17 -0800

    print c0mb 0x01

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,18 +10,22 @@ int main(void)
 
 
 	int i, j, k;
-	
+
 	for (i = 0; i < 10; i++)
 	{	putchar(i  + '0');
-
 		{
 			for (j = 0; j < 10; j++)
 		putchar(j + '0');
 		{
-			for(k = 10; k < 10; k++)
-				putchar(k + '0');
+		for (k = 10; k < 10; k++)
+			putchar(k + '0');
+		
+		if (k < 10)
+			{
 			putchar(',');
 			putchar(' ');
+
+			}
 		}
 		putchar('\n');
 		}
