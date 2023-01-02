17dc16fc9a0f55f61485043d8acc8127dccef4c5
arveyk <harveykisiangani@gmail.com>
2023-01-02 13:18:32 +0300

    strncat checked by Betty

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include <string.h>
 /**
- * *_strcat - combines two strings
+ * *_strncat - combines two strings
  *
  * Return: combined string
  * @dest: pointer to 1st string.
  * @src: pointer to 2nd string.
+ * @n: integer to be used in checking size of array
  */
 char *_strncat(char *dest, char *src, int n)
 {
