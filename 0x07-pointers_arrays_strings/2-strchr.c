425d3ea30463f3415dc64e441ea39779c0dd32e4
arveyk <harveykisiangani@gmail.com>
2023-01-03 15:06:44 +0300

    betty checks 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,16 +4,16 @@
 /**
  * _strchr - locate the character c in string s.
  *
- * Return: pointer to the first occurence of 
+ * Return: pointer to the first occurence of
  * @s: string to be searched
  * @c: character to be found.
  */
 char *_strchr(char *s, char c)
 {
-	
-	for(; *s++ == c++; )
-			if (*s != c)
-			return(NULL);
+
+	for (; *s++ == c++; )
+		if (*s != c)
+			return (NULL);
 
 	return (s);
 }
