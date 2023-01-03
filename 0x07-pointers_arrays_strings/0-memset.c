bc7f94136e9b1d488a6964541b247380428a47dc
arveyk <harveykisiangani@gmail.com>
2023-01-03 20:37:03 +0300

    0.01 memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,13 +5,20 @@
  * _memset - fill memory with constant byte
  *
  * Return: character pointer
- * @s: source 
+ * @s: source
  * @b: do know yet
  * @n: stores the number of bytes to be printed
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	while (sizeof(s) != n)
-		*(s + 1) = b;
+	unsigned int counter = 0;
+
+	while (counter <= n)
+	{
+
+		*s = b;
+		counter = counter + sizeof(char);
+		s++;
+	}
 	return (s);
 }
