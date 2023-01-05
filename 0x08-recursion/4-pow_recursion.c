c100515da59ad984e40225a87cf092639d5aff12
arveyk <harveykisiangani@gmail.com>
2023-01-06 02:51:02 +0300

    exponential potential pow function

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,9 +13,11 @@ int _pow_recursion(int x, int y)
 
 	if (y < 0)
 		return (-1);
-	if (y-- == 0)
-		return (x);
-	return (x * _pow_recursion(x, y));
+	if (y > 0)
+	{
+		x *= x;
+		return (_pow_recursion(x, (y - 1)));
+	}
 
 	return (x);
 }
