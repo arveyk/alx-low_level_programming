56851e42321f06dc967ff53c1ab6c511da16be45
arveyk <harveykisiangani@gmail.com>
2023-01-12 13:42:55 +0300

    malloc created concat function, 0.0 shorter version

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * str_concat -concatenates two strings
@@ -13,20 +14,18 @@
 char *str_concat(char *s1, char *s2)
 {
 
-	char *q, *w;
+	char *q;
 	int g;
-	int len = 0;
-	int set = 0;
+
+	int len1 = strlen(s1);
+	int len2 = strlen(s2);
+	int set = len1 + len2 + 1;
 
 
 	if (*s1 == '\0' || *s2 == '\0')
 		return (NULL);
-	for (g = 0; s1[g] != '\0'; g++)
-	{
-		len++;
-		set++;
-	}
-	q = malloc(sizeof(char) * len);
+
+	q = malloc(sizeof(char) * set);
 
 	if (!q)
 		return (NULL);
@@ -34,19 +33,13 @@ char *str_concat(char *s1, char *s2)
 	for (g = 0; s1[g] != '\0'; g++)
 		q[g] = s1[g];
 
-	for (g = 0; s2[g] != '\0'; g++)
-		len++;
-
-	w = realloc(q, (len * sizeof(char)));
-
-	if (!w)
-		return (NULL);
 
 	for (g = 0; s2[g] != '\0'; g++)
-		w[set++] = s2[g];
+		q[len1 + g] = s2[g];
 
-	w[len] = '\0';
 
-	return (w);
+	q[set - 1] = '\0';
+
+	return (q);
 
 }
