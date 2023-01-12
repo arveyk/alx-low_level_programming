01454ffaae2c22fe10d613c9301b478f172adedf
arveyk <harveykisiangani@gmail.com>
2023-01-12 13:59:06 +0300

    NULL string considered

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,19 +22,16 @@ char *str_concat(char *s1, char *s2)
 	int set = len1 + len2 + 1;
 
 
-	if (*s1 == '\0' || *s2 == '\0' || len1 == 0 || len2 == 0)
-		return (NULL);
-
 	q = malloc(sizeof(char) * set);
 
 	if (!q)
 		return (NULL);
 
-	for (g = 0; s1[g] != '\0'; g++)
+	for (g = 0; g < len1; g++)
 		q[g] = s1[g];
 
 
-	for (g = 0; s2[g] != '\0'; g++)
+	for (g = 0; g < len2; g++)
 		q[len1 + g] = s2[g];
 
 
