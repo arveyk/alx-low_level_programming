c57b13f75404a7e47efbe0b5281d3a986928dac7
arveyk <harveykisiangani@gmail.com>
2023-01-18 19:07:05 +0300

    comment on the function like macro added

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,13 @@
 #ifndef _3_FUNCTION_LIKE_MACRO_H
 #define _3_FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) (x < 0 ? -x : x)
+/**
+ * ABS - computes the absolute value of  a number
+ * @x: the number to be evaluated
+ *
+ * Return: no return statement
+ */
+
+#define ABS(x) (x < 0 ? -x : +x)
+
 #endif
