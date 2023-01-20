c802b90dd64c74f40a0af7a4f864a4eeb632f544
arveyk <harveykisiangani@gmail.com>
2023-01-20 22:16:04 +0300

    0x0A-argv_argc add numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,38 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+
 /**
  * main - sums arguments if they are numbers
+ * @argc: count of the number of parameters
+ * @argv: pointer to the arguments
  *
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
 	int add = 0;
-	int x, p;
+	int x, p = 0;
 
 	if (argc == 1)
 	{
 		printf("0\n");
+		exit(1);
 	}
-	else if (argc < 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else if (!isdigit(atoi(argv[1])) || !isdigit(atoi(argv[2])))
-	{
-		printf("Error\n");
-		return (1);
-	}
-
 	else
 	{
-		p = atoi(argv[1]);
-		x = atoi(argv[2]);
-		add += (p + x);
-	printf("%d\n", add);
+		for (x = 1; x < argc; x++)
+	{
+		p = (atoi(argv[x]));
+		add += p;
+
+		if (!p)
+		{
+			printf("Error\n");
+			exit(1);
+		}
 	}
+	}
+	printf("%d\n", add);
 
 	return (0);
 }
