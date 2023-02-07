d565963a36b3db53fb706d8b9fcfa0a6833dd31c
arveyk <harveykisiangani@gmail.com>
2023-02-07 16:16:35 +0300

    0x14 error checking task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,6 +14,9 @@ int get_bit(unsigned long int n, unsigned int index)
 	bin_rep *y = (bin_rep *)malloc(sizeof(bin_rep));
 	int i = 0;
 
+	if (!y)
+		return (-1);
+
 	while (n != (n / 2))
 	{
 		y->a[i] = n % 2;
