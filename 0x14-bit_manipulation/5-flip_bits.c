f58496e1607c6b5057b8eab72072a13ba8810167
arveyk <harveykisiangani@gmail.com>
2023-02-07 17:12:11 +0300

    0x14 5-flip % and / removed

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,25 +12,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 
 	unsigned int count = 0;
-	bin_rep *h = (bin_rep *)malloc(sizeof(bin_rep));
-	bin_rep *b = (bin_rep *)malloc(sizeof(bin_rep));
-	int i = 0;
-
-	if (!h || !b)
-		return (-1);
 	
-	while (n && m)
-	{
-		h->a[i] = n % 2;
-		n /= 2;
-		b->a[i] = m % 2;
-		m /= 2;
+	if (n ^ m)
+		count += 1;
+	else 
+		count += 0;
 
-		if (h->a[i] == b->a[i])
-			count += 1;
-		i++;
-	}
-	free(h);
-	free(b);
 	return (count);
 }
