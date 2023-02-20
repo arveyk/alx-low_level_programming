b11225b650d9183d2131a93dc685d32f77d0c4b0
arveyk <harveykisiangani@gmail.com>
2023-02-20 19:01:25 +0300

    0x05 puthalf, k var = strlen(s)\2

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,26 +3,31 @@
 
 /**
  * puts_half - print second half of string
+ * @str: pointer to string of numbers to be printed
  *
  * Return: none
- * @str: pointer to string of numbers to be printed
  */
 void puts_half(char *str)
 {
 	int g = strlen(str);
 	int k;
-	char j = *(str + 5);
 
-	if (g / 2 == 0)
+	if (g % 2 == 0)
 	{	k = g / 2;
-		while (k++ < g)
-			_putchar(j++);
+		while (k < g)
+		{
+			_putchar(str[k]);
+			k++;
+		}
 	}
 	else
 	{
 		k = (g - 1) / 2;
-		while (k++ < (g - 1))
-			_putchar(j++);
+		while (k < g)
+		{
+			_putchar(str[k]);
+			k++;
+		}
 	}
 
 	_putchar('\n');
