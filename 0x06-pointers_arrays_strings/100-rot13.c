8350ca64e8f39becb79fcea5eb50bc5892d13779
arveyk <harveykisiangani@gmail.com>
2023-02-21 15:47:18 +0300

    0x06 rotten 13 if cond reduced to one

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,25 +7,25 @@
  * *rot13 - used rot-13 cipher to encrypt string
  *
  * Return: result of encryption
- * @y: pointer to string to be encrypted
+ * @x: pointer to string to be encrypted
  */
-char *rot13(char *y)
+char *rot13(char *x)
 {
-
+	char *y = x;
 	int b = 0;
-
-	if (y == NULL)
-	{
-		return (NULL);
-	}
 
 
 		while (y[b++] != '\0')
 		{
 				if ((y[b] >= 'a' && y[b] <= 'm') || (y[b] >= 'A' && y[b] <= 'M'))
 					y[b] += 13;
-				if ((y[b] >= 'n' && y[b] <= 'Z') || (y[b] >= 'N' && y[b] <= 'Z'))
-					y[b] -= 13;
+		
+		}
+		
+		for (b = 0;(y[b] >= 'n' && y[b] <= 'Z') || (y[b] >= 'N' && y[b] <= 'Z');)
+		{
+			y[b] -= 13;
+			b++;
 		}
 	return (y);
 
