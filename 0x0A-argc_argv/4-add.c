bd69f7f2c129b594bda888fc0eef875f82d44cb5
arveyk <harveykisiangani@gmail.com>
2023-02-27 16:13:48 +0300

    0x0A add numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,24 +14,22 @@ int main(int argc, char *argv[])
 	int add = 0;
 	int x, p = 0;
 
-	if (argc == 1)
+	if (argc <= 1)
 	{
 		printf("0\n");
-		exit(1);
+		return (1);
 	}
-	else
-	{
-		for (x = 1; x < argc; x++)
+
+	for (x = 1; x < argc; x++)
 	{
 		p = (atoi(argv[x]));
-		add += p;
 
 		if (!p)
 		{
 			printf("Error\n");
-			exit(1);
+			return (1);
 		}
-	}
+		add += p;
 	}
 	printf("%d\n", add);
 
