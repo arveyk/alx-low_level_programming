35e4de3d6ebe998bab302d490b2945a5521805e2
arveyk <harveykisiangani@gmail.com>
2023-02-27 21:13:20 +0300

    malloc(sizeof(int) * (max + 1) +1 added

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	arr = (int *)malloc(sizeof(int) * max);
+	arr = malloc(sizeof(int) * (max + 1));
 
 	if (!arr)
 		return (NULL);
