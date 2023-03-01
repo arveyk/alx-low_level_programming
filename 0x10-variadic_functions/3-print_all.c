bee0e22898262577b8678d6f67f9d8d69394c2fe
arveyk <harveykisiangani@gmail.com>
2023-03-01 18:22:07 +0300

    3 printall variadically

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,40 +11,41 @@
 void print_all(const char * const format, ...)
 {
 
-	char *c;
+	char c;
 	int i;
-	float f;
-	char * s;
+	double f;
+	char *s;
+
+	char *fmt = (char *)format;
 	
 	va_list lizts;
 
 	va_start(lizts, format);
-	while(format)
-	{
-		c = va_arg(lizts, char *);
-		if (c)
-			printf("%s", c);
-		else{
-		s = va_arg(lizts,char *);
-		if (s)
-			printf("%s", s);
+	while (*fmt)
+	{	switch (*fmt)
+		{
+			case 'c':
+				c = va_arg(lizts, int);
+				printf("%c", c);
+				break;
+			case 'i':
+				i = va_arg(lizts, int);
+				printf("%d", i);
+				break;
+			case 'f':
+				f = va_arg(lizts, double);
+				printf("%f", f);
+				break;
+			case 's':
+				s = va_arg(lizts, char *);
+				printf("%s", s);
+				break;
+			default:
+				break;
 		}
+		++fmt;
 	}
-
-	while(format)
-	{
-	
-		i = va_arg(lizts, int);
-		if (i)
-			printf("%d", i);
-		else {
-		f = va_arg(lizts, double);
-		if (s)
-			printf("%f", f);
-		}
-	}
-
-
 	va_end(lizts);
+	printf("\n");
 	
 }
