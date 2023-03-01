f9a708a7fcac7f63c4bc46e2acb6336884c78eee
CHARLES G <harveykisiangani@gmail.com>
2023-03-01 22:22:04 +0300

    count for read included

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,23 +4,25 @@
 #include <stdio.h>
 
 /**
- * read_text_file - reads a text file and prints stdout
+ * read_textfile - reads a text file and prints stdout
  * @filename: pointer to the file
- * @letter: size of letter to be read and printed
+ * @letters: size of letter to be read and printed
  *
- * Return 0 if file cannot be opened or read,
+ * Return: 0 if file cannot be opened or read,
  * if filname = NULL, write fails
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 
-	FILE* ptr = fopen(filename, "r");
-	
+	FILE *ptr = fopen(filename, "r");
+	ssize_t count;
+
 	if (ptr == NULL)
 		return (0);
 	if (filename == NULL)
-		return (0);	
-	if (write(1, ptr, letters) == -1)
+		return (0);
+	count = write(1, ptr, letters);
+	if (count == -1)
 		return (0);
 	fclose(ptr);
 	return (0);
