570f8a0353f81da6148a2db1d02b5ce34e21dbd4
CHARLES G <harveykisiangani@gmail.com>
2023-03-02 21:36:13 +0300

    malloc use to create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,41 +13,13 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *ptr, *ptr2;
-	unsigned long x;
-	dog_t n_dog;
-	dog_t *gg;
+	dog_t *n_dog = malloc(sizeof(dog_t));
 
-	gg = &n_dog;
+	n_dog->name = name;
+	n_dog->owner = owner;
 
+	n_dog->age = age;
 
-	ptr = (char *)malloc(sizeof(char) * strlen(name));
-	if (!ptr)
-		free(ptr);
-
-	for (x = 0; x < strlen(name); x++)
-		ptr[x] = name[x];
-	if (!ptr)
-	{
-		return (NULL);
-	}
-
-	ptr2 = (char *)malloc(sizeof(char) * strlen(owner));
-	if (!ptr2)
-		free(ptr2);
-	for (x = 0; x < strlen(owner); x++)
-		ptr2[x] = owner[x];
-
-	if (!ptr2)
-	{
-		return (NULL);
-	}
-
-	n_dog.age = age;
-	n_dog.name = ptr;
-	n_dog.owner = ptr2;
-
-	return (gg);
-	return (0);
+	return (n_dog);
 
 }
