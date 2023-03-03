cf072ab71e2ce18d6c57ff73fb597aada68549b6
CHARLES G <harveykisiangani@gmail.com>
2023-03-03 17:31:51 +0300

    failure handling added

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,12 +15,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_n = (listint_t *)malloc(sizeof(listint_t));
 	listint_t *ptr = *(head);
 	listint_t *temp = NULL;
-
 	unsigned int k = 0;
 
+	if (!new_n)
+		return (NULL);
 	for (; k < idx - 1; k++)
+	{
 		ptr = ptr->next;
-
+		if (!ptr)
+			return (NULL);
+	}
 	temp = ptr->next;
 	ptr->next = new_n;
 	new_n->next = temp;
