26cd861688c5a7b7965a65f6c4e48a443f87b667
CHARLES G <harveykisiangani@gmail.com>
2023-03-20 13:18:57 +0300

    Dynamic lib created and shared

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints length of string  by dynamic call to _strlen
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+
+	printf("%d\n", _strlen("My Dyn Lib"));
+	return (EXIT_SUCCESS);
+}
