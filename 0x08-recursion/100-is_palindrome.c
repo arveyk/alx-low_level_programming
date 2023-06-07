c24928aac1df150ab0a50c572798654692a10acf
CHARLES G <harveykisiangani@gmail.com>
2023-06-08 00:08:39 +0300

    is palindrome with odd return values

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,1 +1,20 @@
 #include "main.h"
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: pointer to the string to be checked
+ *
+ * Return: 1 if it is a palindrome, 0 if not
+ */
+int is_palindrome(char *s)
+{
+
+	int p = strlen(s) - 1;
+	if (s == NULL)
+		return 1;
+	if (s[0] != s[p])
+		return (0);
+	return (s[0] + s[p - 1]);
+}
