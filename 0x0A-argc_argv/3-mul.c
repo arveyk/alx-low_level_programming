6042eac261b34a1582cecd3129f4f8e6bdc37e6a
CHARLES G <harveykisiangani@gmail.com>
2023-06-10 01:28:21 +0300

    trial on the conversion project

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - prints is name to stdout
@@ -9,6 +10,20 @@
  */
 int main(int argc, char *argv[])
 {
-
+	
+	int mult = 1;
+	int count;
+	
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+		for (count = 1; count < argc; count++)
+		{
+			mult *=  atoi(argv[count]);
+		}
+	printf("%d\n", mult);
 	return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - prints is name to stdout
@@ -9,6 +10,26 @@
  */
 int main(int argc, char *argv[])
 {
+
+	int count = 1;
+	int sum = 0;
+
+	if (argc == 0)
+		printf("0\n");
+	else
+	{
+
+		for (; count < argc; count++)
+		{
+			sum += atoi(argv[count]);
+			if (!sum)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
 
 	return (0);
 }
