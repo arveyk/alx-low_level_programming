b637dfd422a6d7efcd8827a9d560458992f3903b
CHARLES G <harveykisiangani@gmail.com>
2023-06-12 16:34:45 +0300

    main files

---
+++ 0x0B-malloc_free/1-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - checks the code for Alx School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+	char *s;
+
+	s = _strdup("ALX SE");
+	if (s == NULL)
+	{
+	
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+	return (0);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,4 @@
+Malloc - is used for dynamic memory allocation
+	this is necessary when the amount of space needed is not known
+
+free - is used to free memory previously allocated by call to malloc or calloc or realloc
