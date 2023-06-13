54c2d00974304a36791e81c7469df67efacae5bd
CHARLES G <harveykisiangani@gmail.com>
2023-06-13 17:11:10 +0300

    strtookenize

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * strtow - splits a sting into words
+ * @str: string to be split
+ *
+ * Return: pointer to array of strings, NULL if str == NULL or "", or failure
+ * occurs
+ */
+
+char **strtow(char *str)
+{
+	char **st_tow;
+	char space[] = " ";
+	int len;
+	int i, j;
+	
+	len = strlen(str) + 1;
+
+	st_tow = malloc(sizeof(int *) * len);
+	if (!st_tow)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+	{
+		st_tow[i] = malloc(sizeof(char) * (len / 3));
+	}
+
+	for (i = 0; i < len; ++i)
+	{
+		for (j = 0; j < (len / 3); ++j)
+		{
+		
+			if ((str[i + 1]) == space[0])
+			{
+				st_tow[i][j + 1] = '\0';
+				continue;
+			}
+			st_tow[i][j] = str[j];
+		}
+	}
+	st_tow[len] = NULL;
+
+	return (st_tow);
+}
