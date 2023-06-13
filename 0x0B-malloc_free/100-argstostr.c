3621a4182b8aa3377d7cc8d8104ba7ecb1b79f57
CHARLES G <harveykisiangani@gmail.com>
2023-06-13 14:28:04 +0300

    print argument

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * argstostr - concatenates all arguments to it
@@ -13,20 +14,29 @@ char *argstostr(int ac, char **av)
 {
 	char *str = NULL;
 	int count;
+	int len;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	str = (char *)malloc(sizeof(char) * ac)
-
 	for (count = 0; count < ac; count++)
+	{
+		len += strlen(av[count]);
+	}
+
+	str = (char *)malloc(sizeof(char ) * len);
+	if (!str)
+		return (NULL);
+
+	for (count = 1; count < ac; count++)
 	{
 		if (*av[count] == '\0')
 		{
 			str[count] = '\n';
 			count++;
 		}
-		str[count] = *av[count];
+		str[count - 1] = *av[count];
 	}
-	return
+	str[count] ='\n';
+	return (str);
 }
