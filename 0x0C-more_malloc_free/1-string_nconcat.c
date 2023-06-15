141dea204b95491070868418d6c309e813dba875
CHARLES G <harveykisiangani@gmail.com>
2023-06-16 00:13:36 +0300

    edge conditions

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,25 +20,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	size_t j;
 	char *combi;
 
-	if (s1 == NULL && s2 == NULL)
-	{
-		combi = "";
-		return (combi);
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	len = strlen(s1) + n + 1;
 	len_s1 = strlen(s1);
 	combi = malloc(sizeof(char) * len);
 	if (combi == NULL)
 		return (NULL);
-	if (s1 == NULL)
-		s1 = "";
-	else if (s2 == NULL)
-		s2 = "";
+
 	for (i = 0; s1 != NULL && i < len_s1; i++)
 		combi[i] = s1[i];
 	for (j = 0; s2 && j < n; j++)
 		combi[i + j] = s2[j];
 	combi[len] = '\0';
-	
+
 	return (combi);
 }
