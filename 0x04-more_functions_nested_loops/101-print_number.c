8a2899d8f4cf0a78dc7af7ac735fb0b417dbcc07
CHARLES G <harveykisiangani@gmail.com>
2023-06-16 17:23:41 +0300

    recursion used to print

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,8 +8,12 @@
  */
 void print_number(int n)
 {
-
-	int y;
-	y = atoi(n);
-	_putchar(y);
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
 }
