2b4b7a213e33cd9c82f9fe9c60aad213e710fd78
CHARLES G <harveykisiangani@gmail.com>
2023-06-16 23:51:42 +0300

    9 times table trial 1

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,16 +12,16 @@ void times_table(void)
 	int prd = 0;
 
 	for (r = 0; r <= 9; r++)
-	{	for (j = 0; j <= 9; j++)
+	{
+		for (j = 0; j <= 9; j++)
 		{
-
-			prd = j * r;
-			_putchar((prd + '0'));
 			if (j < 9)
 			{_putchar(',');
 			_putchar(' ');
 			}
-
+			prd = j * r;
+			_putchar((prd / 10) + '0');
+			_putchar((prd % 10) + '0');
 		}
 	_putchar('\n');
 	}
