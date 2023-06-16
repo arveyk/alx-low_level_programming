3f4b64b03dc0fc46562a2bb7a053f0334671b3e0
CHARLES G <harveykisiangani@gmail.com>
2023-06-16 17:40:49 +0300

    FizzBuzz, one character less printed, space

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,22 +11,19 @@ int main(void)
 
 	for (v = 1; v <= 100; v++)
 	{
+		if (v < 100)
+			printf(" ");
 
 	if ((v % 5 == 0) && (v % 3 == 0))
-		printf("FizzBuzz ");
+		printf("FizzBuzz");
 	else
 		if (v % 3 == 0)
-		printf("Fizz ");
+		printf("Fizz");
 	else
 		if (v % 5 == 0)
-		printf("Buzz ");
+		printf("Buzz");
 	else
-	{
 		printf("%d", v);
-		if (v < 100)
-		printf(" ");
-	}
-
 	}
 	printf("\n");
 
