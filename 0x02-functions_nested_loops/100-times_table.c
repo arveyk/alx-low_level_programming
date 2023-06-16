ff8a8c07918d3dd1d931060a67ef006d08208e4d
CHARLES G <harveykisiangani@gmail.com>
2023-06-16 23:56:11 +0300

    times table file name changed

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * times_table - prints the times table for 9
+ *
+ * Return: none
+ */
+void times_table(void)
+{
+
+	int r, j;
+	int prd = 0;
+
+	for (r = 0; r <= 9; r++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (j < 9)
+			{_putchar(',');
+			_putchar(' ');
+			}
+			prd = j * r;
+			_putchar((prd / 10) + '0');
+			_putchar((prd % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
