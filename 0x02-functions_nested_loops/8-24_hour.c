744de11319d5cf8018cd6426cf2c01aa86e96af7
CHARLES G <harveykisiangani@gmail.com>
2023-06-16 21:33:42 +0300

    jack bauer, putchar with nested loop

--- 0x02-functions_nested_loops/8-24_hour.c
+++ 0x02-functions_nested_loops/8-24_hour.c
@@ -1,93 +1,26 @@
-#include "stdio.h"
 #include "main.h"
 
 /**
- * initializes the countdown sequence to be printed
+ * jack_bauer - prints every minute of a day of Jack bauer
  *
- * Return: Nothing.
+ * Return: None
  */
 void jack_bauer(void)
 {
 
-	int cloe;
-	int jack = 0;
+	int y;
+	int z;
 
-	for (cloe = 0; cloe <= 24 && jack <= 60; ++cloe, jack++)
+	for (y = 0; y < 24; y++)
 	{
-		/*_putchar(10 * (cloe + 0)); */
-
-		printf("%d:", cloe);
-			/*_putchar(10 * (jack + 0)); */
-		printf("%d\n", jack);
-		/*		      break;
-			case 3:
-			      _putchar('a');
-			      break;
-			case 4:
-			      _putchar('b');
-			      break;
-			case 5:
-			      _putchar('c');
-			      break;
-			case 6:
-			      _putchar('b');
-			      break;
-			case 7:
-			      _putchar('c');
-			      break;
-			case 8:
-			      _putchar('d');
-			      break;
-			case 9:
-			      _putchar('e');
-			      break;
-			case 10:
-			      _putchar('f');
-			      break;
-			case 11:
-			      _putchar('g');
-			      break;
-			case 12:
-			      _putchar('h');
-			      break;
-			case 13:
-			      _putchar('i');
-			      break;
-			case 14:
-			      _putchar('j');
-			      break;
-			case 15:
-			      _putchar('k');
-			      break;
-			case 16:
-			      _putchar('l');
-			      break;
-			case 17:
-			      _putchar('m');
-			      break;
-			case 18:
-			      _putchar('n');
-			      break;
-			case 19:
-			      _putchar('o');
-			      break;
-			case 20:
-			      _putchar('p');
-			      break;
-			case 21:
-			      _putchar('q');
-			      break;
-			case 22:
-			      _putchar('r');
-			      break;
-			case 23:
-			      _putchar('s');
-			      break;
-			case 24:
-			      _putchar('t');
-			      break;
-
-			*/
-
+		for (z = 0; z < 60; z++)
+		{
+			_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+			_putchar(':');
+			_putchar((z / 10) + '0');
+			_putchar((z % 10) + '0');
+			_putchar('\n');
+		}
 	}
 }
