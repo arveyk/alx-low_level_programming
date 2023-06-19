29e958aec185dded9550c82545d1bd70f60e01de
CHARLES G <harveykisiangani@gmail.com>
2023-06-19 17:19:39 +0300

    dog struct creates

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,10 @@
+Structures are compound data structure. They are composed of multiple data types under one name.
+The keyword struct is used to create new structures.
+It defines a new data types
+eg.
+typdef struct pin point;
+point{
+int p;
+char i;
+char *c;
+};
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines dog structure
+ * @name: the name of the dog
+ * @age: age of the dog
+ * @owner: who owns the dog
+ *
+ * Description: What else is there to describe, Dogs have
+ * different names and age
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
