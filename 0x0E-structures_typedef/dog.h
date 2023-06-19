18ecd8e1aed5f11897af8b8badd1a7b28d4f129a
CHARLES G <harveykisiangani@gmail.com>
2023-06-19 18:18:16 +0300

    typedef for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,16 +11,16 @@
  * different names and age
  */
 
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/* dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d); */
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
