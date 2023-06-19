73153375795fa6a008c19f50f1341e31f18f7c8c
CHARLES G <harveykisiangani@gmail.com>
2023-06-19 18:10:42 +0300

    if age i <= 0

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,11 @@ void print_dog(struct dog *d)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", (*d).name);
-	printf("Age: %f\n", d->age);
+
+	if (d->age <= 0)
+		printf("Age: (nil)");
+	else
+		printf("Age: %f\n", d->age);
 	if ((*d).owner == NULL)
 		printf("Owner: (nil)\n");
 	else
