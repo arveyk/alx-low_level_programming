180d3b9c9603eaa19011b9048f656b8a431d0c5a
CHARLES G <harveykisiangani@gmail.com>
2023-06-21 22:46:21 +0300

    if name or pointer are null

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include <stdio.h>
+#include <stddef.h>
 
 /**
  * print_name - prints a name
@@ -10,6 +11,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
