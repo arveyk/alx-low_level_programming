d0a4a4b5053863f6cb37c5e875703e9e8c91575c
CHARLES G <harveykisiangani@gmail.com>
2023-06-22 18:27:11 +0300

    else add with caution

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -38,13 +38,10 @@ void print_all(const char * const format, ...)
 				break;
 			case 's':
 				s = va_arg(ap, char *);
-				switch (s)
-				{
-					case NULL:
-						printf("(nil)");
-					default:
-						printf("%s", s);
-				}
+				if (s == NULL)
+					printf("(nil)");
+				else
+					printf("%s", s);
 				break;
 		}
 		if (step > 0 && step < len_form - 1)
