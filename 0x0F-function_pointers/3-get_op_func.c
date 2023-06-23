abfa256f038de10ca846fbdf1f03eb018327a71f
CHARLES G <harveykisiangani@gmail.com>
2023-06-23 10:30:58 +0300

    string length of operator must be 1

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+#include <string.h>
 #include <stddef.h>
 
 /**
@@ -21,7 +22,7 @@ int (*get_op_func(char *s))(int a, int b)
 	int i;
 
 	i = 0;
-	while ((ops[i]).op != NULL)
+	while ((ops[i]).op != NULL && (strlen(s) == 1))
 	{
 		if (*s == *((ops[i]).op))
 			return ((ops[i]).f);
