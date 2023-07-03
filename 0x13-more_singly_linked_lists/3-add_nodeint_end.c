12b967f010ed76c971b9dee9bee2c5ac29a04a03
arveyk <harveykisiangani@gmail.com>
2023-07-03 05:25:06 -0700

    emacs webter eddited

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * add_nodeint_end - adds a node at the end of a listint_t list
+ * @head: pointer to the 1st node's pointer
+ * @n: data for new node
+ * 
+ * Return: address of new node
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+  
+}
