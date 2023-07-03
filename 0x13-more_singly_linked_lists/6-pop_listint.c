a41f647211e2a00c71d0c4cf035845ea89987752
CHARLES G <harveykisiangani@gmail.com>
2023-07-03 20:50:37 +0300

    pop-top if h == NULL, 7get success

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,6 +13,8 @@ int pop_listint(listint_t **head)
 	int data;
 	listint_t *temp;
 
+	if (head == NULL)
+		return (0);
 	temp = (*head)->next;
 	data = (*head)->n;
 	free(*head);
---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * get_nodeint_at_index - returns the node at a give index
+ * @head: pointer to the head node
+ * @index: index of node to search
+ *
+ * Return: Pointer to required index
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+
+	listint_t *trav;
+	listint_t *req_node;
+	unsigned int req = 0;
+
+	if (head == NULL)
+		return (NULL);
+	trav = head;
+	while (trav != NULL)
+	{
+
+		if (req == index)
+		{
+			req_node = trav;
+			break;
+		}
+		req++;
+		trav = trav->next;
+	}
+	return (req_node);
+}
