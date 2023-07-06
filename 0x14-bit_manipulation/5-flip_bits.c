82e7b61da473695d07e4dc23900798254c1145aa
CHARLES G <harveykisiangani@gmail.com>
2023-07-06 17:58:07 +0300

    flips to conversion

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts the number of bits needed to flip to get
+ * to another
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of flips required
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
+	unsigned long int bits_t;
+	unsigned int count = 0;
+	int i = 0;
+
+	bits_t = n ^ m;
+	while (i < 32)
+	{
+		if ((1 << i) & bits_t)
+			count++;
+		i++;
+	}
+	return (count);
+}
