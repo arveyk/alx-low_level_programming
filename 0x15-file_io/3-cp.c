94790a6d501cfbc5674cd5244f263aba463fad2f
CHARLES G <harveykisiangani@gmail.com>
2023-07-12 15:24:10 +0300

    cp,  betty not check

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,7 +5,7 @@
 #include "main.h"
 
 /**
- * main - checks the code
+ * main - copies a files contents into another
  * @ac: argument counter
  * @av: argument vector
  *
@@ -13,7 +13,8 @@
  */
 int main(int ac, char **av)
 {
-	int res = 0;
+	int b_read = 0, b_write;
+	int nb = 0;
 	int fd_to;
 	int fd_from;
 	char buf[1024];
@@ -21,24 +22,40 @@ int main(int ac, char **av)
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(1);
+		exit(97);
 	}
-
-	if (!av[1])
+	fd_from = open(av[1], O_RDONLY);
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_from < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	fd_from = open(av[1], O_RDONLY);
-	fd_to = open(av[2], O_WRONLY | O_CREAT, 0664);
-	if (!fd_to)
+	if (fd_to < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-
-	read(fd_from, buf, 1024);
-	res += write(fd_to, buf, 1024);
+	while (b_read != 0)
+	{
+		b_read = read(fd_from, buf, 1024);
+		if (b_read < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[2]);
+			exit(98);
+		}
+		b_write = 0;
+		do
+		{
+			nb += write(fd_to, &buf[b_write], b_read - b_write);
+			if (nb < 0)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+				exit(99);
+			}
+			b_write += nb;
+		} while (b_write < b_read);
+	}
 	if (!close(fd_from))
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
