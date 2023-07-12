5e1f498a850932fe259d9ec5f5cc87ed0ec570d8
CHARLES G <harveykisiangani@gmail.com>
2023-07-12 13:35:27 +0300

    text appended, return value changed

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,25 +14,23 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	int len;
 	int ext = 0;
 	int count = 0;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd < 0)
 		return (-1);
 	if (text_content == NULL)
 		return (1);
-	len = strlen(text_content);
-	while (text_content[ext] != '\0' && ext < len)
+	while (text_content[ext] != '\0')
 	{
 		count += write(fd, &text_content[ext], 1);
+		if (count < 0)
+			return (-1);
 		ext++;
 	}
-	if (count < 0)
-		return (-1);
-	return (count);
+	return (1);
 
 }
