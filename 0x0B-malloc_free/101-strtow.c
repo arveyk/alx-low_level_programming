d2fb13c0fdd2ec499340714d828dc2cb159647c0
Harvey W. Kis <harveykisiangani@gmail.com>
2023-07-18 15:44:19 +0300

    tokenizer fixed, betty check not yet

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,45 +3,76 @@
 #include <stdlib.h>
 
 /**
+ * ischar - checks if a charcter is printable
+ * @cha: character to check
+ *
+ * Return: 1 on success, 0 on falure
+ */
+int ischar(char cha)
+{
+	if (cha >= 33 && cha <= 126)
+		return (1);
+	return (0);
+}
+/**
  * strtow - splits a sting into words
  * @str: string to be split
  *
  * Return: pointer to array of strings, NULL if str == NULL or "", or failure
  * occurs
  */
-
 char **strtow(char *str)
 {
-	char **st_tow;
+	char **str_arr = NULL;
 	char empty[] = "";
-	char space[] = " ";
 	int len;
-	int i, j;
+	int elem_num = 0, posi_n;
+	int i, m, z, p;
 
 	if (str == NULL || str == empty)
 		return (NULL);
-	len = strlen(str) + 1;
-
-	st_tow = malloc(sizeof(int *) * len);
-	if (!st_tow)
-		return (NULL);
-
-	for (i = 0; i < len; i++)
+	for (i = 0; str[i] != '\0' && str; i++)
 	{
-		st_tow[i] = malloc(sizeof(char) * (len / 3));
-	}
-
-	for (i = 0; i < len; ++i)
-	{
-		for (j = 0; j < (len / 3); ++j)
+		if (ischar(str[i]) == 1)
 		{
-
-			if ((str[i]) == space[0])
-				continue;
-			st_tow[i][j] = str[j];
+			elem_num++;
+			i++;
+			while (str && ischar(str[i]) != 0)
+				i++;
+		}
+		else
+			continue;
+	}
+	elem_num++;
+	str_arr = malloc(elem_num * sizeof(char *));
+	if (!str_arr)
+		return (NULL);
+	str_arr[elem_num - 1] = NULL;
+	m = 0;
+	for (z = 0; z < elem_num && str[m] != '\0'; m++)
+	{
+		if (ischar(str[m]) != 1)
+			continue;
+		else
+		{
+			posi_n = m;
+			len = 0;
+			while (ischar(str[m]) == 1)
+			{
+				len++;
+				m++;
+			}
+			str_arr[z] = malloc((len + 1) * sizeof(**str_arr));
+			if (!str_arr[z])
+				return (NULL);
+			str_arr[z][len] = '\0';
+			p = 0;
+			for (; p < len && str; p++, posi_n++)
+			{
+				str_arr[z][p] = str[posi_n];
+			}
+			z++;
 		}
 	}
-	st_tow[len] = NULL;
-
-	return (st_tow);
+	return (str_arr);
 }
