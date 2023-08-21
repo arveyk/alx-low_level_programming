4ef5885672f5f6021aa10832c0d89dfb79fb4427
Harvey W. Kis <harveykisiangani@gmail.com>
2023-08-21 16:53:36 +0300

    add to python C functions

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,57 @@
+/**
+ * add - adds two integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of subtraction
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of multipliction
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - does division operation
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - gets the remainder of division of a number by another
+ * @a: first number
+ * @b: second number
+ *
+ * Return: modulus or remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
