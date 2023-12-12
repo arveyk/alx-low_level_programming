19e325d8ecd67def580a592564b9abbf43821fa9
Harvey W. Kis <harveykisiangani@gmail.com>
2023-12-12 15:03:44 +0300

    jump search

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * smaller - returns the smaller of 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the smaller number
+ */
+size_t smaller(size_t a, size_t b)
+{
+	if (a > b)
+		return (b);
+	return (a);
+}
+/**
+ * jump_search - searches for an element using jump search algorithm
+ * @array: array to search
+ * @size: number of element in the array
+ * @value: element to search
+ *
+ * Return: index of position if found, else -1
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t left = 0;
+	size_t right = sqrt(size);
+
+	while ((array[smaller(right, size) - 1]) < value)
+	{
+		left = right;
+		right = right + sqrt(size);
+		if (left >= size)
+			return (-1);
+	}
+	while (array[left] < value)
+	{
+		left += 1;
+		if (left == fmin(right, size))
+			return (-1);
+	}
+	if (array[left] == value)
+		return (left);
+	return (-1);
+}
