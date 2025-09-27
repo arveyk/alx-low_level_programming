fab7c29de7f085d5ea92d2b3535bbd914e710e93
arveyk <harveykisiangani@gmail.com>
2025-09-27 22:30:40 +0300

    Removed abs value converter

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,10 +19,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 
-	if (n < 0)
-		z = -n % 10;
-	else
-		z = n % 10;
+	z = n % 10;
 
 	if (z < 6 && z != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, z);
